0b79538e32dfd6f882d9889b7b0d9860fb47fb23
zsyang <1270927087@qq.com>
2020-11-05 15:46:52 +0800

    汇率计算

--- pb4.py
+++ pb4.py
@@ -5,6 +5,10 @@
 1.1691港元
 0.1318欧元
 """
+
+from python_basics.pb11 import func
+from python_basics import pb11
+
 dollar = 0.1491
 yen = 16.6955
 pound = 0.1139
@@ -13,3 +17,6 @@ euro = 0.1318
 a = "你好吗?"
 b = a.replace("吗", "").replace("?", "!")
 print(b)
+
+pb11.func()
+func()
